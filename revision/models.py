28791eae2f85fe4f2a44b2133d5e0dc349e5e442
mohamedSaber <mm@frfg.drfg>
2023-08-10 16:27:37 +0300

    o

---
+++ revision/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Arnaba(models.Model):
+    TYPE_CHOICES = (
+        ('vocabulary', 'vocabulary'),
+        ('expression', 'expression'),
+    )
+
+    STATE_CHOICES = (
+        ('hot', 'hot'),
+        ('cold', 'cold'),
+    )
+
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=200,unique=True)
+    DE = models.CharField(max_length=2000,unique=True)
+    EN = models.CharField(max_length=2000,unique=True)
+    revision_number = models.IntegerField(default=0)
+    state = models.CharField(max_length=200, choices= STATE_CHOICES, default='hot')
+    type = models.CharField(max_length=200, choices= TYPE_CHOICES, default='expression')
+
+    
---
+++ revision/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path
+from . import views
+app_name = 'revision'
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('arnaba_create', views.arnaba_create, name='arnaba_create'),
+    # path('arnab_create', views.arnab_create, name='arnab_create'),
+    # path('arnaba_list', views.arnaba_list, name='arnaba_list'),
+    # path('arnab_list', views.arnab_list, name='arnab_list'),
+    # path('update/<int:id>/', views.arnaba_update, name='arnaba_update'),
+    # path('gass/<int:id>/', views.arnaba_gass, name='arnaba_gass'),
+    # path('males/update/<int:id>/', views.arnab_update, name='arnab_update'),
+    # path('delete/<int:id>/', views.arnaba_delete, name='arnaba_delete'),
+    # path('males/delete/<int:id>/', views.arnab_delete, name='arnab_delete'),
+    # path('<int:id>/', views.detail, name='detail'),
+    # path('males/<int:id>/', views.arnab_detail, name='arnab_detail')
+
+
+
+]
